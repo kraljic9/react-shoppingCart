72c12c0c09816e235e0e782b87cc18f9dfcc7eab
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-01-07 20:58:21 +0100

    Added variables for cart and products

--- shopping-cart/src/ShoppingCart.jsx
+++ shopping-cart/src/ShoppingCart.jsx
@@ -1,3 +1,27 @@
-function ShoppingCart() {}
+/*
+
+Products
+Cart
+
+add
+increase
+decrease
+remove at 0
+total
+*/
+
+import { useState } from "react";
+
+function ShoppingCart() {
+  const products = [
+    { id: 1, text: "T-shirt", price: 20 },
+    { id: 2, text: "Hoodie", price: 50 },
+    { id: 3, text: "Pants", price: 30 },
+  ];
+
+  const [cart, setCart] = useState([]);
+
+  return <></>;
+}
 
 export default ShoppingCart;
