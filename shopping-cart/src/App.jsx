1bc8c47b28fbf8710fd6aedeaa694e7b91fe5b0e
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-01-07 20:57:40 +0100

    Displayed ShoppinCart.jsx

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
 import ShoppingCart from "./ShoppingCart";
 
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ShoppingCart />
+    </>
+  );
 }
 
 export default App;
